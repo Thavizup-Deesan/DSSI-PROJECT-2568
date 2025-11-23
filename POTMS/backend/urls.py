d2f74ef2eba0edb8cd5e57d6a76a7851194ece9c
beckham-4567 <thavizup.de.66@ubu.ac.th>
2025-11-23 15:37:16 +0700

    DSSI-PROJECT-2568

--- POTMS/backend/urls.py
+++ POTMS/backend/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('master-data/', TemplateView.as_view(template_name='S08_Master_Data.html'), name='master_data'),
+    path('', TemplateView.as_view(template_name='S08_Master_Data.html'), name='master_data'),
     
 ]
